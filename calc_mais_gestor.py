c375235fd9097827e1a8cfa69f89467a7ef57c02
davisxavier1984 <davisxavier1984@gmail.com>
2025-02-04 19:57:53 -0300

    Update calc_mais_gestor.py

--- calc_mais_gestor.py
+++ calc_mais_gestor.py
@@ -189,6 +189,8 @@ if 'valor_acs' not in st.session_state:
     st.session_state['valor_acs'] = 0.0
 if 'valor_estrategicas' not in st.session_state:
     st.session_state['valor_estrategicas'] = 0.0
+if 'ied' not in st.session_state:
+    st.session_state['ied'] = 0.0
 if 'calculo_realizado' not in st.session_state:
     st.session_state['calculo_realizado'] = False  # Inicializado como False
 
